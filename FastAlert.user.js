eb909affc1ca886e5932682ec4686c55dc3988e0
Amanpreet Singh <coool.aman@gmail.com>
2020-02-23 10:02:52 +0530

    Added Get Alerts

--- FastAlert.user.js
+++ FastAlert.user.js
@@ -161,6 +161,31 @@ function equities() {
     //Wait For Alert Bell
     waitEE('.add-alert-bell', () => {
         captureToken();
+        getAlerts(GM_getValue(tokenKey))
+    });
+}
+
+function getAlerts(token) {
+    fetch("https://in.investing.com/alerts/instrument-data?pair_ID=17984", {
+        "credentials": "include",
+        "headers": {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0",
+            "Accept": "application/json, text/javascript, /; q=0.01",
+            "Accept-Language": "en-US,en;q=0.5",
+            "is-service": "1",
+            "ref-uri": "/equities/adani-enterprises",
+            "token": token,
+            "X-Requested-With": "XMLHttpRequest"
+        },
+        "referrer": "https://in.investing.com/equities/adani-enterprises",
+        "method": "GET",
+        "mode": "cors"
+    }).then((response) => {
+        return response.text();
+    }).then((text) => {
+        return console.log('Alerts', text);
+    }).catch(function (error) {
+        console.log('Request failed', error)
     });
 }
 
