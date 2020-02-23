2a85ebd01c83ec0fc48cfe3887af4569ab7aa7fd
Amanpreet Singh <coool.aman@gmail.com>
2020-02-23 10:13:45 +0530

    Moved Get Alerts to Lib. Json Parsing of alerts.

--- FastAlert.user.js
+++ FastAlert.user.js
@@ -161,33 +161,10 @@ function equities() {
     //Wait For Alert Bell
     waitEE('.add-alert-bell', () => {
         captureToken();
-        getAlerts(GM_getValue(tokenKey))
+        getAlerts(17984, GM_getValue(tokenKey))
     });
 }
 
-function getAlerts(token) {
-    fetch("https://in.investing.com/alerts/instrument-data?pair_ID=17984", {
-        "credentials": "include",
-        "headers": {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0",
-            "Accept": "application/json, text/javascript, /; q=0.01",
-            "Accept-Language": "en-US,en;q=0.5",
-            "is-service": "1",
-            "ref-uri": "/equities/adani-enterprises",
-            "token": token,
-            "X-Requested-With": "XMLHttpRequest"
-        },
-        "referrer": "https://in.investing.com/equities/adani-enterprises",
-        "method": "GET",
-        "mode": "cors"
-    }).then((response) => {
-        return response.text();
-    }).then((text) => {
-        return console.log('Alerts', text);
-    }).catch(function (error) {
-        console.log('Request failed', error)
-    });
-}
 
 function captureToken() {
     XMLHttpRequest.prototype.realSetRequest = XMLHttpRequest.prototype.setRequestHeader;
@@ -395,7 +372,7 @@ function updateAlertSummary(m) {
     let ltp = readLtp();
     //Search Symbol
     searchSymbol(getMappedTicker(), function (top) {
-        let alrts = m[top.pair_ID];
+        let alrts = getAlerts(top.pair_ID, GM_getValue(tokenKey));
         altz.innerHTML = ""; //Reset Old Alerts
         if (alrts) {
             alrts.sort(((a, b) => {
