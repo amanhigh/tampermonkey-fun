894a5eeeb2a6e403c67582f3ec3958357f0151d7
Amanpreet Singh <coool.aman@gmail.com>
2021-05-16 00:22:50 +0530

    Added Auto Cutoff and Search

--- Imdb.user.js
+++ Imdb.user.js
@@ -21,35 +21,73 @@
 //Selectors
 const movieTitleSelector = '.TitleHeader__TitleText-sc-1wu6n3d-0';
 const movieRatingSelector = 'span.AggregateRatingButton__RatingScore-sc-1il8omz-1';
+const myImdbRatingSelector = 'span.UserRatingButton__RatingScore-sc-15ploje-1';
+const languageSelector = 'li[data-testid="title-details-languages"] a';
 
 //Events
 const imdbFilterKey = "imdbFilterKey"
 
 //***************IMDB ********************
-GM_registerMenuCommand("Filter", () => {
-    imdbFilterFire("en")
-});
+GM_registerMenuCommand("Filter", () => {imdbFilterFire("Filter")});
+GM_registerMenuCommand("YSearch", () => {imdbFilterFire("YSearch")});
+GM_registerMenuCommand("XSearch", () => {imdbFilterFire("XSearch")});
+GM_registerMenuCommand("Youtube Full", () => {imdbFilterFire("Youtube Full")});
+
 
 GM_addValueChangeListener(
     imdbFilterKey, (keyName, oldValue, newValue) => {
-        console.log(newValue);
-        imdbFilter(newValue.lang)
+        switch (newValue.command) {
+            case "Filter":
+                imdbFilter();
+                break;
+            case "YSearch":
+                YSearch(getName());
+                break;
+            case "XSearch":
+                XSearch(getName());
+                break;
+            case "Youtube Full":
+                YoutubeSearch(getName() + " Full Movie");
+                break
+            default:
+                alert("Invalid Command: " + newValue.command)
+        }
     });
 
 
-function imdbFilterFire(lang) {
-    GM_setValue(imdbFilterKey, {lang: lang, date: new Date()});
+function imdbFilterFire(cmd) {
+    GM_setValue(imdbFilterKey, {command: cmd, date: new Date()});
 }
 
-function imdbFilter(lang) {
-    console.log(lang)
-    let name = $(movieTitleSelector).text();
+function imdbFilter() {
+    let name = getName();
+    let lang = $(languageSelector).text();
     let rating = parseFloat($(movieRatingSelector).text());
-    let cutoff = 7;
+    let myRating = parseFloat($(myImdbRatingSelector).text());
+    let cutoff = getCutoff(lang);
 
     if (rating < cutoff) {
         GM_notification(`${rating} < ${cutoff} (${lang}) discarded`, name);
         window.close();
+    } else if (myRating > 0) {
+        GM_notification(`Movie Watched: ${myRating} discarded`, name);
+        window.close();
     } else
         YoutubeSearch(name + " trailer")
+}
+
+function getName() {
+    return $(movieTitleSelector).text();
+}
+
+function getCutoff(lang) {
+    switch (lang) {
+        case "Punjabi":
+        case "Hindi":
+            return 5;
+        case "English":
+            return 7;
+        default:
+            alert("Unknown Language: " + lang)
+    }
 }
