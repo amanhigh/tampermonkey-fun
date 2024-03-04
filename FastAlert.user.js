cfa73123e041b02e62aa47966831aaa76063197f
Amanpreet Singh <coool.aman@gmail.com>
2024-03-04 19:22:28 +0530

    Extracted Actions to Seperate File

--- FastAlert.user.js
+++ FastAlert.user.js
@@ -17,6 +17,7 @@
 // @require      lib/ui.js
 // @require      lib/client/kite.js
 // @require      lib/client/investing.js
+// @require      lib/sites/tv/action.js
 // @require      lib/sites/tv/alert.js
 // @require      lib/sites/tv/const.js
 // @require      lib/sites/tv/core.js
