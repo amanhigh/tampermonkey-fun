ab1f47b3d901a179b57ec2dd821f5a6b381dd6c7
Amanpreet Singh <coool.aman@gmail.com>
2024-11-24 17:22:44 +0530

    Finished action.js

--- manager/tv_action.js
+++ manager/tv_action.js
@@ -12,6 +12,55 @@ class TradingViewActionManager {
     }
 
     /**
+     * Execute ReasonPrompt function which disables SwiftKeys, prompts for reasons,
+     * and enables SwiftKeys after the prompt.
+     *
+     * @param {function} callback - The callback function to be executed with the reason returned from SmartPrompt
+     * @return {void} 
+     */
+    reasonPrompt(callback) {
+        //Disable SwiftKeys
+        this.toggleSwiftKeys(false);
+
+        //Prompt
+        SmartPrompt(Constants.TRADING.PROMPT.REASONS, Constants.TRADING.PROMPT.OVERRIDES).then((reason) => {
+            callback(reason);
+
+            //Enable SwiftKeys
+            this.toggleSwiftKeys(true);
+        });
+    }
+
+    /**
+     * Copies the given text to the clipboard and displays a message.
+     *
+     * @param {string} text - The text to be copied to the clipboard
+     * @return {undefined} 
+     */
+    clipboardCopy(text) {
+        GM_setClipboard(text);
+        this.message(`ClipCopy: ${text}`, 'yellow');
+    }
+
+    /**
+     * Function to clear all items.
+     */
+    clearAll() {
+        this.domManager.waitJClick(Constants.DOM.SIDEBAR.DELETE_ARROW, () => {
+            this.domManager.waitJClick(Constants.DOM.SIDEBAR.DELETE_DRAWING);
+        });
+    }
+
+    /**
+     * Sets focus on the input element with the specified ID.
+     *
+     * @return {void} 
+     */
+    focusCommandInput() {
+        $(`#${Constants.UI.IDS.INPUTS.COMMAND}`).focus();
+    }
+
+    /**
      * Opens Current Ticker Relative to Benchmark.
      * Eg. Stock to Nifty, Crypto to Bitcoin etc
      */
@@ -64,5 +113,14 @@ class TradingViewActionManager {
         } else if (!swiftEnabled && document.title.includes('SwiftKeys')) {
             document.title = document.title.replace(liner, '');
         }
+    }
+
+    /**
+     * Displays a message with the given text and color
+     * @param {string} text - The message text
+     * @param {string} color - The message color
+     */
+    message(text, color) {
+        console.log(`%c${text}`, `color:${color};`);
     }
 }
