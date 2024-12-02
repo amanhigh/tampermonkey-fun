634a319be0206f46b2d1e6f7a2f3cb16486e9f87
Amanpreet Singh <coool.aman@gmail.com>
2024-12-02 21:29:27 +0530

    Added Inputs

--- src/core/barkat.ts
+++ src/core/barkat.ts
@@ -10,17 +10,81 @@ export class Barkat {
     this.setupUI();
   }
 
+  // eslint-disable-next-line max-lines-per-function
   setupUI() {
-    this.uiUtil.buildArea(Constants.UI.IDS.AREAS.MAIN, '76%', '6%').appendTo('body');
+    const $area = this.uiUtil.buildArea(Constants.UI.IDS.AREAS.MAIN, '76%', '6%');
+    $area.appendTo('body');
 
     this.uiUtil
       .buildWrapper(Constants.UI.IDS.AREAS.TOP)
-      .appendTo(`#${Constants.UI.IDS.AREAS.MAIN}`)
+      .appendTo($area)
+      .append(
+        this.uiUtil.buildCheckBox(Constants.UI.IDS.CHECKBOXES.SWIFT, false).change(() => {
+          console.log('Enabling swift key');
+        })
+      )
       .append(
         this.uiUtil.buildButton(Constants.UI.IDS.BUTTONS.SEQUENCE, 'S', () => {
-          console.log('Sequence Switched');
+          console.log('Handling sequence switch');
+        })
+      )
+      .append(
+        this.uiUtil.buildButton(Constants.UI.IDS.BUTTONS.REFRESH, 'R', () => {
+          console.log('Handling refresh button');
+        })
+      )
+      .append(
+        this.uiUtil.buildButton(Constants.UI.IDS.BUTTONS.ALERT_CREATE, 'A', () => {
+          console.log('Handling alert create button');
+        })
+      )
+      .append(
+        this.uiUtil.buildButton(Constants.UI.IDS.BUTTONS.JOURNAL, 'J', () => {
+          console.log('Handling journal button');
+        })
+      )
+      .append(
+        this.uiUtil.buildCheckBox(Constants.UI.IDS.CHECKBOXES.RECENT, false).change(() => {
+          console.log('Handling recent ticker reset');
+        })
+      )
+      .append(this.uiUtil.buildWrapper(Constants.UI.IDS.AREAS.SUMMARY));
+
+    this.uiUtil
+      .buildWrapper(Constants.UI.IDS.AREAS.MID)
+      .appendTo($area)
+      .append(
+        this.uiUtil.buildButton(Constants.UI.IDS.INPUTS.DISPLAY, '', () => {
+          console.log('Handling display input');
+        })
+      )
+      .append(
+        this.uiUtil.buildButton(Constants.UI.IDS.INPUTS.COMMAND, '', () => {
+          console.log('Handling command input');
         })
       );
+
+    this.uiUtil.buildWrapper(Constants.UI.IDS.AREAS.ALERTS).appendTo($area);
+    this.uiUtil.buildWrapper(Constants.UI.IDS.AREAS.ORDERS).appendTo($area);
+    this.uiUtil.buildWrapper(Constants.UI.IDS.AREAS.JOURNAL).hide().appendTo($area);
+  }
+
+  journalUI() {
+    this.uiUtil
+      .buildWrapper(`${Constants.UI.IDS.AREAS.JOURNAL}-type`)
+      .appendTo(`#${Constants.UI.IDS.AREAS.JOURNAL}`)
+      .append(
+        this.uiUtil.buildButton('trend', 'TR', () => {
+          console.log('Recording journal trend');
+        })
+      )
+      .append(
+        this.uiUtil.buildButton('ctrend', 'CT', () => {
+          console.log('Recording journal counter-trend');
+        })
+      );
+
+    this.uiUtil.buildWrapper(Constants.UI.IDS.AREAS.AUDIT).appendTo(`#${Constants.UI.IDS.AREAS.JOURNAL}`);
   }
 }
 
