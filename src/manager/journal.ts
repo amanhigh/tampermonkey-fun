384fd0e87ed00a90a8ff9568e0df645b83febe8e
Amanpreet Singh <coool.aman@gmail.com>
2026-04-22 21:51:03 +0530

    No Fast

--- src/manager/journal.ts
+++ src/manager/journal.ts
@@ -81,7 +81,6 @@ export class JournalManager implements IJournalManager {
 
     for (const position of [0, 1, 2, 3]) {
       this.timeframeManager.applyTimeFrame(position);
-      await this.waitForScreenshotUpdate();
       const timeframe = this.sequenceManager.sequenceToTimeFrameConfig(sequence, position).symbol;
       const order = position + 1;
 
@@ -125,9 +124,5 @@ export class JournalManager implements IJournalManager {
     const date = `${now.getFullYear()}${String(now.getMonth() + 1).padStart(2, '0')}${String(now.getDate()).padStart(2, '0')}`;
     const time = `${String(now.getHours()).padStart(2, '0')}${String(now.getMinutes()).padStart(2, '0')}`;
     return `${date}_${time}`;
-  }
-
-  private async waitForScreenshotUpdate(): Promise<void> {
-    await new Promise<void>((resolve) => setTimeout(resolve, 200));
   }
 }
